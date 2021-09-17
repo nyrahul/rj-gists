41c67358aeb576abf9a6835a77f85d4a5d69cb44
Rahul Jadhav <nyrahul@gmail.com>
2021-09-18 00:02:48 +0530

    updates

---
+++ cilium/delete_all_unmanaged_pods.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Delete all unmanaged pods
+
+kubectl get pods --all-namespaces -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name,HOSTNETWORK:.spec.hostNetwork --no-headers=true | grep '<none>' | awk '{print "-n "$1" "$2}' | xargs -L 1 -r kubectl delete pod
